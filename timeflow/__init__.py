b177749422661f666500711dc32fced52c633a71
Philip Schleihauf <uniphil@gmail.com>
2013-02-08 13:35:53 -0500

    reorganize

--- timeflow/__init__.py
+++ timeflow/__init__.py
@@ -1,115 +1,11 @@
 from abc import ABCMeta, abstractmethod
 from numpy import ndarray
 
-
-class DataConnector(object):
-    """"The connector for sequences of routines.
-    Attaches to the acting routine, and references another routine by label.
-    """
-    routine_registry = {} # mutable struture is the same for all instances
-
-    def __init__(self, source, **source_modifiers):
-        self.source_label = source
-        self.source_modifiers = source_modifiers
-
-    @classmethod
-    def register(cls, label, routine):
-        if label in cls.routine_registry:
-            raise KeyError('Routine label conflict for {}.'.format(label))
-        cls.routine_registry[label] = routine
-
-    def get(self):
-        # handle merges and all that...
-        try:
-            source_routine = self.routine_registry[self.source_label]
-        except KeyError:
-            raise KeyError('The routine "{}" is not registered. '
-                'Registered routines: {}'.format(self.source_label,
-                ', '.join(self.routine_registry.keys())))
-        source_data = source_routine.get_all()
-        return source_data
+from timeflow.routine_bases import RoutineBase
 
 
-class RoutineBase(object):
-    __metaclass__ = ABCMeta
-
-    def __init__(self, label, data_from, **source_modifiers):
-        self._data_source = DataConnector(data_from, **source_modifiers)
-        DataConnector.register(label, self)
-
-    @property
-    def data(self):
-        return self._data_source.get()
-
-    @abstractmethod
-    def operate(self, *args, **kwargs):
-        pass
-
-    def operate_if(self, *args, **kwargs):
-        """Be optimistic that the operate method just happens to return a
-        suitable boolean column"""
-        condition = self.operate(self, *args, **kwargs)
-        return condition
-
-    def get_new(self, *args, **kwargs):
-        columns = self.operate(*args, **kwargs)
-        return columns
-
-    def get_all(self, *args, **kwargs):
-        columns = self.get_new(*args, **kwargs)
-        full = self.data # link the data and new columns
-        return full
-
-    def get_if(self, *args, **kwargs):
-        condition = self.operate_if(*args, **kwargs)
-        compressed = self.data.compress(condition)
-        return compressed
-
-        
-class FileSource(object):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def get(self):
-        return open(self.filename, 'r')
-            
-
-class FileReadBase(RoutineBase):
-    """Special class opens files.
-    Does not connect to a source with a DataConnector.
-    """
-    __metaclass__ = ABCMeta
-
-    def __init__(self, label, data_from):
-        DataConnector.register(label, self)
-        self._data_source = FileSource(data_from)
-
-    def get_new(self, *args, **kwargs):
-        raise NotImplemented
-
-    def get_all(self, *args, **kwargs):
-        return self.operate(*args, **kwargs)
-
-    def get_if(self, *args, **kwargs):
-        raise NotImplemented
-
-
-
-class CSVImport(FileReadBase):
-    """This import class expects a file-like stream as its source"""
-    def __init__(self, map=None, const=None, **kwargs):
-        super(CSVImport, self).__init__(**kwargs)
-        self._map = map
-        self.const = const
-
-    def operate(self):
-        from numpy import genfromtxt
-        csv_config = {
-            'delimiter': ',',
-            'names': True,
-            'dtype': None,
-        }
-        return genfromtxt(self.data, **csv_config)
+class MapColumns(RoutineBase):
+    pass
 
 
 class BoolFilter(RoutineBase):
@@ -130,7 +26,7 @@ class BoolFilter(RoutineBase):
 
     def operate(self):
         column = self.data[self.thresh_column]
-        result = self.cmp_trans[self.thresh_operator](self.thresh_value, column)
+        result = self.cmp_trans[self.thresh_operator](column, self.thresh_value)
         return result
 
 
@@ -142,36 +38,6 @@ if __name__ == '__main__':
                          column='temperature', operator='<', value=15.3)
 
     print imp.get_all()
-    print bfilter.get_all()
+    print bfilter.get_if()
     print bfilter.data.dtype
-
-
-class File(RoutineBase):
-    """Simple class for handling file reading in a timeflow"""
-    def __init__(self, filename, **kwargs):
-        self.filename = filename
-
-    def timeseries(self):
-        raise NotImplementedError('This file routine does not provide a '\
-            'timeseries directly. File.stream() will give you a file object.')
-
-    def stream(self):
-        return open(self.filename)
-
-
-class CSV(File):
-    def __init__(self, step_meta, data_from):
-        self.data_from = data_from.stream
-
-    def timeseries(self):
-        from numpy import genfromtxt
-        csv_config = {
-            'delimiter': ',',
-            'names': True,
-            'dtype': None,
-        }
-        return genfromtxt(self.data_from(), **csv_config)
-
-    def stream(self): pass
-
 
--- timeflow/data.py
+++ timeflow/data.py
@@ -1,3 +1,93 @@
 """
-
+do stuff with data
 """
+
+class LabeledRegistry(dict):
+    """like a dict, but if a keyerror is raised, it will try to import it as
+    a module."""
+    pass
+
+
+routines = LabeledRegistry()
+
+
+class Connector(object):
+    """"The connector for sequences of routines.
+    Attaches to the acting routine, and references another routine by label.
+    If the label is not in the registry, see if we can import it as a filename.
+    """
+
+    def __init__(self, source, source_modifiers):
+        self.source_label = source
+        self.source_modifiers = source_modifiers
+
+    @classmethod
+    def register(cls, label, routine):
+        if label in routines:
+            raise KeyError('Routine label conflict for {}.'.format(label))
+        routines[label] = routine
+
+    def get(self):
+        # handle merges and all that...
+        try:
+            source_routine = routines[self.source_label]
+        except KeyError:
+            raise KeyError('The routine "{}" is not registered and could not'
+                'be imported. Registered routines: {}'.format(self.source_label,
+                ', '.join(routines.keys())))
+        source_data = source_routine.get_all()
+        return source_data
+
+
+
+class FileSource(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get(self):
+        return open(self.filename, 'r')
+            
+
+class FileReadBase(RoutineBase):
+    """Special class opens files.
+    Does not connect to a source with a DataConnector.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, label, data_from):
+        DataConnector.register(label, self)
+        self._data_source = FileSource(data_from)
+
+    def get_new(self, **kwargs):
+        raise NotImplemented
+
+    def get_all(self, **kwargs):
+        return self.operate(**kwargs)
+
+    def get_if(self, **kwargs):
+        raise NotImplemented
+
+
+class CSVImport(FileReadBase):
+    """This import class expects a file-like stream as its source"""
+    def __init__(self, map=None, const=None, **kwargs):
+        super(CSVImport, self).__init__(**kwargs)
+        self._map = map
+        self.const = const
+
+    def operate(self):
+        from numpy import genfromtxt
+        csv_config = {
+            'delimiter': ',',
+            'names': True,
+            'dtype': None,
+        }
+        return genfromtxt(self.data, **csv_config)
+
+
+class FileSaveBase(RoutineBase):
+
+    def __init__(self, label, data_from, save_to, source_modifiers=None):
+        self._data_source = DataConnector(data_from, source_modifiers)
+        self.filename = save_to
+
