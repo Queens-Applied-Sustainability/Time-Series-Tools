0febd3f2b5e6c106bfeff04dc97bb5fd7cfe680e
Philip Schleihauf <uniphil@gmail.com>
2013-02-08 15:55:16 +0000

    very vague csvimport

--- timeflow/__init__.py
+++ timeflow/__init__.py
@@ -2,16 +2,15 @@ from abc import ABCMeta, abstractmethod
 from numpy import ndarray
 
 
-class DataConnector(ndarray):
+class DataConnector(object):
     """"The connector for sequences of routines.
     Attaches to the acting routine, and references another routine by label.
     """
     routine_registry = {} # mutable struture is the same for all instances
 
-    def __init__(self, source, merge=None, **modifiers):
+    def __init__(self, source, **source_modifiers):
         self.source_label = source
-        self.merge = merge
-        self.modifiers = modifiers
+        self.source_modifiers = source_modifiers
 
     @classmethod
     def register(cls, label, routine):
@@ -21,16 +20,21 @@ class DataConnector(ndarray):
 
     def get(self):
         # handle merges and all that...
-        source_routine = self.routine_registry[self.source_label]
+        try:
+            source_routine = self.routine_registry[self.source_label]
+        except KeyError:
+            raise KeyError('The routine "{}" is not registered. '
+                'Registered routines: {}'.format(self.source_label,
+                ', '.join(self.routine_registry.keys())))
         source_data = source_routine.get_all()
-        return source_data
+        return source_data.get_all()
 
 
 class RoutineBase(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, label, data_from, merge, **modifiers):
-        self._data_source = DataConnector(data_from, **modifiers)
+    def __init__(self, label, data_from, **source_modifiers):
+        self._data_source = DataConnector(data_from, **source_modifiers)
         DataConnector.register(label, self)
 
     @property
@@ -73,16 +77,38 @@ class BoolFilter(RoutineBase):
         '!=': lambda l, r: l != r,
     }
 
-    def __init__(self, data_from, column, rule, value):
-        super(BoolFilter, self).__init__(data_from)
+    def __init__(self, column, operator, value, **kwargs):
+        super(BoolFilter, self).__init__(**kwargs)
         self.thresh_column = column
-        self.thresh_rule = rule
+        self.thresh_operator = operator
         self.thresh_value = value
 
     def operate(self):
         column = self.data[self.thresh_column]
-        result = cmp_trans[self.thresh_rule](self.thresh_value, column)
+        result = cmp_trans[self.thresh_operator](self.thresh_value, column)
         return result
+
+
+class CSVImport(RoutineBase):
+    """This import class expects a file-like stream as its source"""
+    def __init__(self, map=None, const=None, **kwargs):
+        super(CSVImport, self).__init__(**kwargs)
+        self.map = map
+        self.const = const
+
+    def operate(self):
+        return ndarray()
+
+
+if __name__ == '__main__':
+
+    imp = CSVImport(label='import', data_from='File',
+                    source_modifiers={'filename': 'time-series.csv'})
+
+    bfilter = BoolFilter(label='bfilt', data_from='import',
+                         column='temperature', operator='<', value=15.3)
+
+    print bfilter.data
 
 
 class File(RoutineBase):
